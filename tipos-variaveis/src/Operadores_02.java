12d1ed490a64807f826bffe4feb289f2a2ee8a83
victorfdiniz <victorfd80@gmail.com>
2024-05-06 22:52:13 -0300

    update

---
+++ tipos-variaveis/src/Operadores_02.java
@@ -0,0 +1,14 @@
+public class Operadores_02 {
+    
+    public static void main(String[] args) {
+        int numero = 5;
+
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println(numero);
+    }
+}
