1f604cd79c268d3896a2bc92eeddd98d950b51a7
victorfdiniz <victorfd80@gmail.com>
2024-05-13 21:42:11 -0300

    Desafio l

--- terminal-e-argumentos/src/AboutMe2.java
+++ terminal-e-argumentos/src/AboutMe2.java
@@ -5,24 +5,25 @@ public class AboutMe2 {
 
     public static void main(String[] args) {
 
-    try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         System.out.println("Digite sua altura");
         double altura = scanner.nextDouble();
 
+        scanner.nextLine();
+
         System.out.println("Digite seu nome");
-        String nome = scanner.next();
+        String nome = scanner.nextLine();
 
         System.out.println("Digite seu sobrenome");
-        String sobrenome = scanner.next();
+        String sobrenome = scanner.nextLine();
 
         System.out.println("Digite sua idade");
         int idade = scanner.nextInt();
 
         // Imprimindo os dados obtidos do usuário
-        System.out.println("Olá, me chamo" + nome + " " + sobrenome);
+        System.out.println("Olá, me chamo " + nome + " " + sobrenome);
         System.out.println("Tenho " + idade + " anos");
         System.out.println("Minha altura é " + altura + "cm ");
+        
+        }
     }
-    
-    }
-}
